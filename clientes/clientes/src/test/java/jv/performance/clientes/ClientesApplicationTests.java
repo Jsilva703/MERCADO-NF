7d7ac079350ac76c9efef784e66cf5c9107201e7
Jsilva703 <joaovictornunessilva12345@gmail.com>
2026-03-16 23:29:06 -0300

    Estrutura de estudo

---
+++ clientes/clientes/src/test/java/jv/performance/clientes/ClientesApplicationTests.java
@@ -0,0 +1,13 @@
+package jv.performance.clientes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ produtos/produtos/src/main/java/jv/performance/produtos/ProdutosApplication.java
@@ -0,0 +1,13 @@
+package jv.performance.produtos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdutosApplication.class, args);
+	}
+
+}
